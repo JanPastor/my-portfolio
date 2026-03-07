b14586f349b695efe2cb8b7b288db402b8ce9916
JanPastor <janpastor123@gmail.com>
2026-03-07 09:19:56 -0800

    Fix blank page by switching to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import CyberBackground from './components/CyberBackground';
 import Home from './pages/Home';
